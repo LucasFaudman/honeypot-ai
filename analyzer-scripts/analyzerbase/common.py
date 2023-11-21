39736c31fdc287873fa1f997259711cd845e74db
Lucas Faudman <lucasfaudman@Lucass-MacBook-Pro.local>
2023-11-20 16:52:10 -0800

    Major improvements to openaianalyzer: added command and malware analysis and question answering function to generate md

--- analyzer-scripts/analyzerbase/common.py
+++ analyzer-scripts/analyzerbase/common.py
@@ -9,7 +9,6 @@ from pprint import pprint
 from typing import Union
 from urllib.parse import urlparse
 
-#MYIPS = os.environ.get("MYIPS", "").split(",")
-MYIPS = "98.159.37.5,216.243.47.166".split(",")
+MYIPS = os.environ.get("MYIPS", "").split(",")
 test_logs_path = pathlib.Path("/Users/lucasfaudman/Documents/SANS/internship/tests/logs")
 test_attacks_path = pathlib.Path("/Users/lucasfaudman/Documents/SANS/internship/tests/attacks")
