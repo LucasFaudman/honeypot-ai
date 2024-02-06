db4266f5bfd464d2a0573c5192c6f999b4465d25
Lucas Faudman <lucasfaudman@Lucass-MacBook-Pro.local>
2024-02-05 20:52:28 -0800

    fix keyfile in install-zeek-on-honeypot.sh

--- setup/install-zeek-on-honeypot.sh
+++ setup/install-zeek-on-honeypot.sh
@@ -3,5 +3,12 @@ HONEYPOT_IP="<HONEYPOT_IP>"
 SSH_PORT="<SSH_PORT>"
 KEYFILE="<KEYFILE>"
 
+if [ ! -z "$KEYFILE" ]; then
+    KEYFILE="-i$KEYFILE"
+else
+    KEYFILE="" 
+    echo "No keyfile provided. Using password authentication."
+fi
+
 scp -P "$SSH_PORT" "$KEYFILE" -r "setup/zeek-setup" "$USER@$HONEYPOT_IP:/home/$USER/zeek-setup"
 ssh -p "$SSH_PORT" "$KEYFILE" "$USER@$HONEYPOT_IP" "cd zeek-setup && chmod +x *.sh && sudo ./install-zeek. && sudo ./run-zeek-as-service.sh"
